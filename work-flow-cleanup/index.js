c4c85199293e7c11c5fa7f3a9b08a4d48bd296ff
Dimuthu <dimuthu@urbanise.com>
2024-06-06 19:31:27 +1000

    New

--- work-flow-cleanup/index.js
+++ work-flow-cleanup/index.js
@@ -3,23 +3,24 @@ const github = require("@actions/github");
 var execSync = require("child_process").execSync;
 
 try {
-  const repositoryPath = core.getInput('repo-path');
+  const repositoryPath = core.getInput("repo-path");
   const time = new Date().toTimeString();
   core.setOutput("time", time);
 
   console.log(`Repo Path: ${repositoryPath}`);
 
-  let pastWorkflows = execSync(
+  let pastWorkflows1 = execSync(
     `gh run list --repo ${repositoryPath} --json name,number,createdAt --limit 10`
   );
-  
+
   const lastDate = new Date();
   lastDate.setDate(lastDate.getDate() - 30);
   console.log("Last Day:" + lastDate.toLocaleDateString());
 
- console.log(`pastWorkflows: ${pastWorkflows}`);
+  const pastWorkflows = JSON.parse(pastWorkflows1);
+  console.log(`pastWorkflows: ${pastWorkflows}`);
   console.log(`pastWorkflows.length: ${pastWorkflows.length}`);
-  
+
   for (workFlow of pastWorkflows) {
     const createdAt = new Date(workFlow.createdAt);
 
