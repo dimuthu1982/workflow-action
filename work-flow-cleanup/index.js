abbbab8d28891fd0498bafcb8812a8779375c428
Dimuthu <dimuthu@urbanise.com>
2024-06-06 17:50:49 +1000

    New

--- work-flow-cleanup/index.js
+++ work-flow-cleanup/index.js
@@ -1,5 +1,7 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const exec = require("child_process").exec;
+
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -9,7 +11,14 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  //console.log(`The event payload: ${payload}`);
+
+
+const { stdout, stderr } = await exec("gh run list --repo dimuthu1982/auto-tag-workflow-testing --json name,number --limit 1");
+
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+
 } catch (error) {
   core.setFailed(error.message);
 }
