f2e5b5904bd5f45d553c84f019811550af10dbb8
dimuthu1982 <dimuthu1982@yahoo.com>
2024-06-06 18:01:14 +1000

    Update index.js

--- work-flow-cleanup/index.js
+++ work-flow-cleanup/index.js
@@ -1,19 +1,20 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const execSync = require("child_process");
+var execSync = require('child_process').execSync;
 
 
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
+  console.log(`Hello V1);
   const time = new Date().toTimeString();
   core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  //console.log(`The event payload: ${payload}`);
 
-let pastWorkflows = execSync('gh run list --repo dimuthu1982/auto-tag-workflow-testing --json name,number --limit 1');
+var options = {
+  encoding: 'utf8'
+};
+  
+let pastWorkflows = execSync('gh run list --repo dimuthu1982/auto-tag-workflow-testing --json name,number --limit 1', options);
   console.log(`pastWorkflows: ${pastWorkflows}`);
 } catch (error) {
   core.setFailed(error.message);
