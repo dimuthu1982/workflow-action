be80811ecf29e169a85d95173d028fd499fab6d9
Dimuthu <dimuthu@urbanise.com>
2024-06-06 18:59:46 +1000

    New

--- work-flow-cleanup/index.js
+++ work-flow-cleanup/index.js
@@ -1,7 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-var execSync = require('child_process').execSync;
-
+var execSync = require("child_process").execSync;
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -10,23 +9,28 @@ try {
   const time = new Date().toTimeString();
   core.setOutput("time", time);
 
-var options = {
-  encoding: 'utf8'
-};
-  
-let pastWorkflows = execSync('gh run list --repo ${repository} --json name,number,createdAt --limit 10');
-console.log(`pastWorkflows: ${pastWorkflows}`);
-const lastDate = new Date();
-lastDate.setDate(lastDate.getDate() - 30);
-console.log("Last Day:" + lastDate.toLocaleDateString());
+  var options = {
+    encoding: "utf8",
+  };
 
-for (workFlow of pastWorkflows) {
-  const createdAt = new Date(workFlow.createdAt);
+  let pastWorkflows = execSync(
+    "gh run list --repo ${repository} --json name,number,createdAt --limit 10"
+  );
+  console.log(`pastWorkflows: ${pastWorkflows}`);
+  const lastDate = new Date();
+  lastDate.setDate(lastDate.getDate() - 30);
+  console.log("Last Day:" + lastDate.toLocaleDateString());
 
-  if(lastDate.getTime() > createdAt.getTime) {
-    console.log("Deleting")
-  } else {
-    console.log(`Skip [name: ${workFlow.name}, Created: ${workFlow.createdAt}, Number: ${workFlow.number}]`)
+  for (workFlow of pastWorkflows) {
+    const createdAt = new Date(workFlow.createdAt);
+
+    if (lastDate.getTime() > createdAt.getTime) {
+      console.log("Deleting");
+    } else {
+      console.log(
+        `Skip [name: ${workFlow.name}, Created: ${workFlow.createdAt}, Number: ${workFlow.number}]`
+      );
+    }
   }
 } catch (error) {
   core.setFailed(error.message);
