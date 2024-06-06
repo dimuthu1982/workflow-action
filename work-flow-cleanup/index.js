583dd327aa19c1361eb5d9fb781b5650fb6ff0be
dimuthu1982 <dimuthu1982@yahoo.com>
2024-06-06 19:01:49 +1000

    Update index.js

--- work-flow-cleanup/index.js
+++ work-flow-cleanup/index.js
@@ -3,20 +3,14 @@ const github = require("@actions/github");
 var execSync = require("child_process").execSync;
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("repository");
-  console.log(`Hello V1`);
+  const repository = core.getInput("repository");
   const time = new Date().toTimeString();
   core.setOutput("time", time);
-
-  var options = {
-    encoding: "utf8",
-  };
 
   let pastWorkflows = execSync(
     "gh run list --repo ${repository} --json name,number,createdAt --limit 10"
   );
-  console.log(`pastWorkflows: ${pastWorkflows}`);
+  
   const lastDate = new Date();
   lastDate.setDate(lastDate.getDate() - 30);
   console.log("Last Day:" + lastDate.toLocaleDateString());
